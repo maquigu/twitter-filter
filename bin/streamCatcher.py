99c51c2a3f3bfb8c14571ca3d9af7acd9820f746
Martín Quiroga <maq@openfilter.co>
2016-05-24 06:32:51 +0000

    adding streamCatcher daemon

---
+++ bin/streamCatcher.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import time
+import sys
+import traceback
+from datetime import datetime
+import simplejson as json
+import logging as log
+import config
+from twitter import *
+from twitter.util import printNicely
+from twitter.stream import TwitterStream, Timeout, HeartbeatTimeout, Hangup
+from pprint import pformat
+from app.models import (
+    Stream,
+    User,
+    Tweet,
+    Hashtag,
+    Mention,
+    URL,
+    Media,
+    db
+)
+
+stream_timeout = 3
+auth = OAuth(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET, config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+
+def _process_tweet(tweet_dict):
+    user_obj = db.session.query(User).filter(User.tw_id==tweet_dict.get('user', {}).get('id_str', None)).first()
+    if user_obj is None:
+        u = tweet_dict['user']
+        user_obj = User(
+            screen_name=u['screen_name'],
+            tw_id=u['id_str'],
+            location=u['location'],
+            json_str=json.dumps(u)
+        )
+    tweet_obj = Tweet(
+        tw_id = tweet_dict['id_str'],
+        created_at = datetime.strptime(tweet_dict['created_at'], '%a %b %d %H:%M:%S +0000 %Y'),
+        json_str = json.dumps(tweet_dict),
+        user = user_obj
+    )
+    for hashtag in tweet_dict['entities']['hashtags']:
+        hashtag_obj = db.session.query(Hashtag).filter(Hashtag.text==hashtag['text']).first()
+        if hashtag_obj is None:
+            hashtag_obj = Hashtag(text=hashtag['text'])
+        tweet_obj.hashtags.append(hashtag_obj)
+    for mention in tweet_dict['entities']['user_mentions']:
+        mention_obj = db.session.query(Mention).filter(Mention.screen_name==mention['screen_name']).first()
+        if mention_obj is None:
+            mention_obj = Mention(
+                tw_id = mention['id_str'],
+                screen_name = mention['screen_name'],
+                name = mention['name']
+            )
+        tweet_obj.mentions.append(mention_obj)
+    for url in tweet_dict['entities']['urls']:
+        url_obj = db.session.query(URL).filter(URL.url==url['url']).first()
+        if url_obj is None:
+            url_obj = URL(
+                url = url['url'],
+                display_url = url['display_url'],
+                expanded_url = url['expanded_url']
+            )
+        tweet_obj.urls.append(url_obj)
+    for media in tweet_dict['entities'].get('media',[]): # media is not always present
+        media_obj = db.session.query(Media).filter(Media.tw_id == media['id_str']).first()
+        if media_obj is None:
+            media_obj = Media(
+                tw_id = media['id_str'],
+                media_url = media['media_url'],
+                display_url = media['display_url']
+            )
+        tweet_obj.media.append(media_obj)
+    db.session.add(tweet_obj)
+    db.session.commit()
+
+def follow_these():
+    follow = []
+    streams = db.session.query(Stream).filter(Stream.active==1).all()
+    if streams is None:
+        return None
+    for stream_obj in streams:
+        for lot_obj in stream_obj.lots:
+            for user_obj in lot_obj.users:
+                follow.append(user_obj.tw_id)
+    sys.stderr.write('Stream catcher following: ' + repr(follow) + '\n')
+    return follow
+
+def catch_streams(timeout):
+    while 1:
+        follow = follow_these()
+        if not follow:
+            sys.stderr.write('No active streams, waiting '+str(timeout)+' secs.\n')
+            time.sleep(timeout)
+            continue
+        follow_csv = ','.join(follow)
+        twitter_stream = TwitterStream(auth=auth, timeout=timeout)
+        last_check = int(time.time())
+        for tweet in twitter_stream.statuses.filter(follow=follow_csv, track=follow_csv):
+            try:
+                if tweet is None:
+                    printNicely("-- None --")
+                elif tweet is Timeout:
+                    printNicely("-- Timeout --")
+                elif tweet is HeartbeatTimeout:
+                    printNicely("-- Heartbeat Timeout --")
+                elif tweet is Hangup:
+                    printNicely("-- Hangup --")
+                elif tweet.get('text'):
+                    printNicely("-- Processing --")
+                    _process_tweet(tweet)
+                else:
+                    sys.stderr.write('Unparsed:\n'+tweet+'\n')
+                now = int(time.time())
+                if now > (last_check + timeout):
+                    last_check = now
+                    new_follow_csv =  ','.join(follow_these())
+                    if new_follow_csv != follow_csv:
+                        sys.stderr.write('New follows ... restarting stream\n')
+                        break
+            except Exception, e:
+                sys.stderr.write(pformat(tweet)+'\n')
+                sys.stderr.write(traceback.format_exc()+'\n')
+
+catch_streams(stream_timeout)
