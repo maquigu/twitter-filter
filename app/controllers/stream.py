65978f5e5f096ca3e4f221fcd9e6fd6d14cd958a
Martín Quiroga <martin@quiroga.co>
2016-08-24 17:01:43 -0700

    abstracting for modularity in app/controllers/stream.py

--- app/controllers/stream.py
+++ app/controllers/stream.py
@@ -95,152 +95,36 @@ def get_stream_tweets(stream_name):
         direction=direction, message='success'
     ) 
 
-def set_filters(stream_name, q):
-    filters = {
-        "stream_name": stream_name,
-    }
-    start_timestamp = request.args.get('start')
-    end_timestamp = request.args.get('end')
-    # These are lists, specified via multiple html params of the same name
-    users = request.args.getlist('user_id')
-    lots = request.args.getlist('lot_id')
-    hashtags = request.args.getlist('hashtag_id')
-    shares = request.args.getlist('share_id')
-    if users:
-        filters["users"] = users
-        q = q.filter(User._id.in_(users))
-    if lots:
-        filters["lots"] = lots
-        q = q.filter(Lot._id.in_(lots))
-    if hashtags:
-        filters["hashtags"] = hashtags
-        q = q.filter(Hashtag._id.in_(hashtags))
-    if shares:
-        filters["shares"] = shares
-        q = q.join(TweetMedia).join(Media)
-        q = q.filter(Media._id.in_(shares))
-    if start_timestamp is not None:
-        filters["start"] = start_timestamp
-        q = q.filter(Tweet.created_at >= start_timestamp)
-    if end_timestamp is not None and not "now":
-        filters["end"] = end_timestamp
-        q = q.filter(Tweet.created_at <= end_timestamp)
-    return q, filters
-
-
 @stream_mod.route('/<stream_name>/user-metrics', methods=['GET'])
 def get_stream_user_metrics(stream_name):
-    metrics = []
-    q = db.session.query(User._id, User.screen_name, func.count(Tweet.tw_id)). \
-        join(Tweet). \
-        join(LotUser). \
-        join(Lot). \
-        join(StreamLot). \
-        join(Stream)
-    q = q.filter(Stream.name == stream_name)
-    q, filters = set_filters(stream_name, q)
-    q = q.group_by(User.screen_name).limit(config.TOP_N)
-    #sys.stderr.write("QUERY: %s\n" % str(q))
-    for r in q.all():
-        #sys.stderr.write("ROW: %s\n" % repr(r))
-        um = {
-            "user_id": r[0],
-            "screen_name": r[1],
-            "tweets": r[2]
-        }
-        metrics.append(um)
+    filters = get_filters(stream_name)
+    metrics = user_metrics(filters)
     return jsonify(
         metrics=metrics, filters=filters, message='success'
     ) 
 
 @stream_mod.route('/<stream_name>/lot-metrics', methods=['GET'])
 def get_stream_lot_metrics(stream_name):
-    metrics = []
-    q = db.session.query(Lot._id, Lot.name, func.count(Tweet.tw_id)). \
-        join(Tweet). \
-        join(User). \
-        join(LotUser). \
-        join(Lot). \
-        join(StreamLot). \
-        join(Stream)
-    q = q.filter(Stream.name == stream_name)
-    q, filters = set_filters(stream_name, q)
-    q = q.group_by(User.screen_name).limit(config.TOP_N)
-    #sys.stderr.write("QUERY: %s\n" % str(q))
-    for r in q.all():
-        #sys.stderr.write("ROW: %s\n" % repr(r))
-        um = {
-            "lot_id": r[0],
-            "lot_name": r[1],
-            "tweets": r[2]
-        }
-        metrics.append(um)
+    filters = get_filters(stream_name)
+    metrics = lot_metrics(filters)
     return jsonify(
         metrics=metrics, filters=filters, message='success'
     ) 
 
 @stream_mod.route('/<stream_name>/hashtag-metrics', methods=['GET'])
 def get_stream_hashtag_metrics(stream_name):
-    metrics = []
-    start_timestamp = request.args.get('start')
-    end_timestamp = request.args.get('end')
-    q = db.session.query(Hashtag._id, Hashtag.text, func.count(Tweet.tw_id)). \
-        join(TweetHashtag). \
-        join(Tweet). \
-        join(User). \
-        join(LotUser). \
-        join(Lot). \
-        join(StreamLot). \
-        join(Stream)
-    q = q.filter(Stream.name == stream_name)
-    if start_timestamp is not None:
-        q = q.filter(Tweet.created_at >= start_timestamp)
-    if end_timestamp is not None and not "now":
-        q = q.filter(Tweet.created_at <= end_timestamp)
-    q = q.group_by(Hashtag.text).limit(config.TOP_N)
-    #sys.stderr.write("QUERY: %s\n" % str(q))
-    for r in q.all():
-        #sys.stderr.write("ROW: %s\n" % repr(r))
-        hm = {
-            "hashtag_id": r[0],
-            "hashtag": r[1],
-            "tweets": r[2]
-        }
-        metrics.append(hm)
+    filters = get_filters(stream_name)
+    metrics = hashtag_metrics(filters)
     return jsonify(
-        metrics=metrics, start=start_timestamp, end=end_timestamp, message='success'
+        metrics=metrics, filters=filters, message='success'
     )
 
 @stream_mod.route('/<stream_name>/url-metrics', methods=['GET'])
 def get_stream_url_metrics(stream_name):
-    metrics = []
-    start_timestamp = request.args.get('start')
-    end_timestamp = request.args.get('end')
-    q = db.session.query(URL._id, URL.expanded_url, func.count(Tweet.tw_id)). \
-        join(TweetURL). \
-        join(Tweet). \
-        join(User). \
-        join(LotUser). \
-        join(Lot). \
-        join(StreamLot). \
-        join(Stream)
-    q = q.filter(Stream.name == stream_name)
-    if start_timestamp is not None:
-        q = q.filter(Tweet.created_at >= start_timestamp)
-    if end_timestamp is not None and not "now":
-        q = q.filter(Tweet.created_at <= end_timestamp)
-    q = q.group_by(URL.expanded_url).limit(config.TOP_N)
-    ##sys.stderr.write("QUERY: %s\n" % str(q))
-    for r in q.all():
-        ##sys.stderr.write("ROW: %s\n" % repr(r))
-        m = {
-            "url_id": r[0],
-            "url": r[1],
-            "tweets": r[2]
-        }
-        metrics.append(m)
+    filters = get_filters(stream_name)
+    metrics = url_metrics(filters)
     return jsonify(
-        metrics=metrics, start=start_timestamp, end=end_timestamp, message='success'
+        metrics=metrics, filters=filters, message='success'
     )
 
 
@@ -264,3 +148,138 @@ def get_stream_tweets_page(stream_name):
         ).paginate(page, per_page, False).items:
         tweets.append(json.loads(tweet_obj.json_str))
     return jsonify(tweets=tweets, message='success') 
+
+# get_filters grabs the filters from request.args
+def get_filters(stream_name):
+    filters = {
+        "stream_name": stream_name,
+    }
+    start_timestamp = request.args.get('start')
+    end_timestamp = request.args.get('end')
+    # These are lists, specified via multiple html params of the same name
+    users = request.args.getlist('user_id')
+    lots = request.args.getlist('lot_id')
+    hashtags = request.args.getlist('hashtag_id')
+    shares = request.args.getlist('share_id')
+    if users:
+        filters["users"] = users
+    if lots:
+        filters["lots"] = lots
+    if hashtags:
+        filters["hashtags"] = hashtags
+    if shares:
+        filters["shares"] = shares
+    if start_timestamp is not None:
+        filters["start"] = start_timestamp
+    if end_timestamp is not None and not "now":
+        filters["end"] = end_timestamp
+    return filters
+    
+# set_query_filters sets the query filters based on above 
+def set_query_filters(q, stream_name=None, users=[], lots=[], hashtags=[], shares=[], start=[], end=[]):
+    if stream_name:
+        q = q.filter(Stream.name == stream_name)
+    if users:
+        q = q.filter(User._id.in_(users))
+    if lots:
+        q = q.filter(Lot._id.in_(lots))
+    if hashtags:
+        q = q.filter(Hashtag._id.in_(hashtags))
+    if shares:
+        q = q.join(TweetMedia).join(Media)
+        q = q.filter(Media._id.in_(shares))
+    if start:
+        q = q.filter(Tweet.created_at >= start_timestamp)
+    if end and end is not "now":
+        q = q.filter(Tweet.created_at <= end_timestamp)
+    return q
+
+def user_metrics(filters):
+    metrics = []
+    q = db.session.query(User._id, User.screen_name, func.count(Tweet.tw_id)). \
+        join(Tweet). \
+        join(LotUser). \
+        join(Lot). \
+        join(StreamLot). \
+        join(Stream)
+    q = set_query_filters(q, **filters)
+    q = q.group_by(User.screen_name).limit(config.TOP_N)
+    #sys.stderr.write("QUERY: %s\n" % str(q))
+    for r in q.all():
+        #sys.stderr.write("ROW: %s\n" % repr(r))
+        um = {
+            "user_id": r[0],
+            "screen_name": r[1],
+            "tweets": r[2]
+        }
+        metrics.append(um)
+    return metrics
+
+def lot_metrics(filters):
+    metrics = []
+    q = db.session.query(Lot._id, Lot.name, func.count(Tweet.tw_id)). \
+        join(LotUser). \
+        join(User). \
+        join(Tweet). \
+        join(StreamLot). \
+        join(Stream)
+    q = set_query_filters(q, **filters)
+    q = q.group_by(User.screen_name).limit(config.TOP_N)
+    for r in q.all():
+        um = {
+            "lot_id": r[0],
+            "lot_name": r[1],
+            "tweets": r[2]
+        }
+        metrics.append(um)
+    return metrics
+
+def hashtag_metrics(filters):
+    metrics = []
+    q = db.session.query(Hashtag._id, Hashtag.text, func.count(Tweet.tw_id)). \
+        join(TweetHashtag). \
+        join(Tweet). \
+        join(User). \
+        join(LotUser). \
+        join(Lot). \
+        join(StreamLot). \
+        join(Stream)
+    q = q.filter(Stream.name == stream_name)
+    q = set_query_filters(q, **filters)
+    q = q.group_by(Hashtag.text).limit(config.TOP_N)
+    #sys.stderr.write("QUERY: %s\n" % str(q))
+    for r in q.all():
+        #sys.stderr.write("ROW: %s\n" % repr(r))
+        hm = {
+            "hashtag_id": r[0],
+            "hashtag": r[1],
+            "tweets": r[2]
+        }
+        metrics.append(hm)
+    return metrics
+
+def url_metrics(filters):
+    metrics = []
+    start_timestamp = request.args.get('start')
+    end_timestamp = request.args.get('end')
+    q = db.session.query(URL._id, URL.expanded_url, func.count(Tweet.tw_id)). \
+        join(TweetURL). \
+        join(Tweet). \
+        join(User). \
+        join(LotUser). \
+        join(Lot). \
+        join(StreamLot). \
+        join(Stream)
+    q = q.filter(Stream.name == stream_name)
+    q = set_query_filters(q, **filters)
+    q = q.group_by(URL.expanded_url).limit(config.TOP_N)
+    for r in q.all():
+        m = {
+            "url_id": r[0],
+            "url": r[1],
+            "tweets": r[2]
+        }
+        metrics.append(m)
+    return metrics
+
+
