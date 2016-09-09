5f40015dcb705fa640b53315d51f014749f4837a
Martín Quiroga <martin@quiroga.co>
2016-09-09 14:36:24 -0700

    debug on

--- bin/twitterBuffer.py
+++ bin/twitterBuffer.py
@@ -8,5 +8,6 @@ from app import app
 # Run the server.
 
 #app.run(host='0.0.0.0', port=8080, debug=True)
+app.debug = True
 server = pywsgi.WSGIServer(('', config.API_PORT), app, handler_class=WebSocketHandler)
 server.serve_forever()
