c4eacf2f4f8916dc2b45b3edf55ab04ce4649ec3
Martín Quiroga <martin@quiroga.co>
2016-09-09 22:50:16 -0700

    trying spinning on server side

--- app/controllers/socket.py
+++ app/controllers/socket.py
@@ -4,6 +4,7 @@ import sys
 from flask import Blueprint, request, jsonify
 import config
 from sqlalchemy import func
+import gevent
 
 # Import models
 from app.models import (
@@ -31,6 +32,27 @@ from app.models import query
 # Define the blueprint: "streams", set its url prefix: app.url/streams
 socket_mod = Blueprint("sockets", __name__, url_prefix="/sockets")
 
+def query_for_new_tweets(ws, filters, max_id, since_id, count, direction):
+    while not ws.closed:
+        tweets, new_max_id, new_since_id = query.filter_tweets(filters, max_id, since_id, count, direction)
+        if new_since_id != 0 and new_max_id !=0:
+            json_out = json.dumps({
+                "filters": filters,
+                "tweets":tweets, 
+                "max_id":max_id, 
+                "since_id":since_id,
+                "direction":direction,
+                "status":"success"
+            })
+            ws.send(json_out)
+            if direction == "new":                
+                max_id = since_id
+        if direction == "new":  
+            gevent.sleep(30)
+        else:
+            return
+
+
 @socket_mod.route("/tweets")
 def get_stream_tweets(ws):
     while not ws.closed:
@@ -48,23 +70,14 @@ def get_stream_tweets(ws):
             since_id = message.get("since_id", None)
             count = message.get("count", config.TWEETS_PER_PAGE)
             direction = message.get("direction", None)
-            tweets, max_id, since_id = query.filter_tweets(filters, max_id, since_id, count, direction)
-            json_out = json.dumps({
-                "filters": filters,
-                "tweets":tweets, 
-                "max_id":max_id, 
-                "since_id":since_id,
-                "direction":direction,
-                "status":"success"
-            })
-            #log.critical("Socket Out: "+repr(json_out))
-            ws.send(json_out)
+            query_for_new_tweets(ws, filters, max_id, since_id, count, direction)
         except Exception, e:
             log.exception("WS Error in tweets: "+repr(e))
             json_out = json.dumps({
                 "status": "error",
                 "message": repr(e)
             })
+            ws.send(json_out)
 
 @socket_mod.route("/metrics")
 def get_stream_metrics(ws):
@@ -94,5 +107,6 @@ def get_stream_metrics(ws):
                 "status": "error",
                 "message": repr(e)
             })
+            ws.send(json_out)
 
 
